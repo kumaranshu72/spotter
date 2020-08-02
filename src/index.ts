52d577cb39471481db642d3179e2d2af9e7c1c60
Anshu <kumaranshu72@gmail.com>
2020-08-02 13:28:49 +0530

    added babel and type script

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+const app = express();
+const port = 3000;
+const sayHelloTo = (name: string): string => `Hello ${name}!`;
+app.get("/", (req, res) => res.send(sayHelloTo("World")));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

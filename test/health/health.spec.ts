5b92b5b79bf0292644feb08a831a7342d0201d2c
Anshu <kumaranshu72@gmail.com>
2020-08-04 17:35:53 +0530

    added test case for health check:

---
+++ test/health/health.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import 'mocha'
+import request from 'supertest'
+
+import app from '../../src/app'
+
+const HEALTH_TEST_URL = '/v1/health'
+
+describe(`GET ${HEALTH_TEST_URL} - Test Cases`, () => {
+  it('should return 200', () => {
+    return request(app)
+      .get(HEALTH_TEST_URL)
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.have.a('object')
+      })
+  })
+})
